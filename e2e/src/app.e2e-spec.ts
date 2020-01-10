b50a59472a35f6288faa641a00b60fc157498774
Pasha Ivanov (Consultant) <c-pasha.ivanov@macc02w42nzhtdd.corp.hulu.com>
2020-01-10 14:19:09 -0800

    .

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('angular-tour-of-heroes-final app is running!');
+    expect(page.getTitleText()).toEqual('heroes app is running!');
   });
 
   afterEach(async () => {
